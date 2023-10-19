f5c12c728ed4d1533a4192de4d83812221dbae2d
jun980724 <sprhtm@naver.com>
2023-10-19 13:14:00 +0000

    plus even_list

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ from typing import List
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
 
-    pass
+    return [num for num in int_list if num % 2 == 0]
 
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
-
 
     pass
 
 def main():
 
-int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even_int_list = even_list(int_list)
-output = sum_of_squares_of_even(even_int_list)
-print(output)
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
 
 if __name__ == "__main__":
-main()
+    main()
 
