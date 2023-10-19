e8829011bfe6615cd6a8d1d2353185980a8551b4
jun980724 <sprhtm@naver.com>
2023-10-19 13:17:47 +0000

    plus sum_of_squares_of_even func

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ def even_list(int_list: List[int]) -> List[int]:
 
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
 
-
-    pass
+    return sum(num * num for num in even_int_list)
 
 def main():
 
-int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even_int_list = even_list(int_list)
-output = sum_of_squares_of_even(even_int_list)
-print(output)
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
 
 if __name__ == "__main__":
-main()
+    main()
 
